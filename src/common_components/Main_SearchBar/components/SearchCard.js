65dba0e6cd72954355c64f63e3957ec7d979ea39
Aryan Personal <aryanpersonal@Aryans-MacBook-Air.local>
2022-04-02 23:38:47 +0100

    checking progress

--- src/common_components/Main_SearchBar/components/SearchCard.js
+++ src/common_components/Main_SearchBar/components/SearchCard.js
@@ -5,13 +5,29 @@ import MovieCategoryIcon from '../../../assets/icon-category-movie.svg'
 import TvSeriesCategoryIcon from '../../../assets/icon-category-tv.svg'
 import Data from '../../../data.json'
 
-function SearchCard() {
+function SearchCard(props) {
 
-    const [count, setCount] = useState()
+    const [bookMark, setBookMark] = useState(props.bookmark)
+
+    const handleBookMark = () => {
+      setBookMark(!bookMark)
+    }
+
 
   return (
-    <div className='search_bar__card'>
-        
+    <div className='search_bar__card_container'>
+        <div className="card">
+          <img src={ props.image } alt="" className='background_image'/>
+          <button className='bookmark' onClick={ handleBookMark }><img src={ bookMark === true ? BookMarkFull : BookMark } alt="" /></button>
+        </div>
+        <div className="info_wrapper">
+          <ul className="information">
+            <li className='text-small'>{ props.year }</li>
+            <li className='text-small'><img src={ props.category === 'Movie' ? MovieCategoryIcon : TvSeriesCategoryIcon } alt="" />{ props.category }</li>
+            <li className='text-small'>{ props.rating }</li>
+          </ul>
+          <h4>{ props.title }</h4>
+        </div>
     </div>
   )
 }
--- src/common_components/Main_SearchBar/components/SearchResult.js
+++ src/common_components/Main_SearchBar/components/SearchResult.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import SearchCard from './SearchCard'
+import Data from '../../../data.json'
+
 
 function SearchResult(props) {
   return (
     <div className='search_result'>
-      <h2>Found 2 results for '{ props.search_value }' </h2>
+      <h2>Found results for '{ props.search_value }' </h2>
+    
+        <div className="search_card_container">
+            
 
+            {
+                Data.filter(data => {
+                    if(data == '') {
+                        return data
+                    } else if(data.title.toLowerCase().includes(props.search_value.toLowerCase())) {
+                        return data
+                    }
+                }).map((data, key) => {
+                    return(
+                        <SearchCard 
+                        key = { key }
+                        title = { data.title }
+                        image = { data.isTrending === true ? data.thumbnail.trending.small : data.thumbnail.regular.small }
+                        year = { data.year }
+                        category = { data.category }
+                        rating = { data.rating }
+                        bookmark = { data.isBookmarked }
+                        />
+                    )
+                })
+            }
+
+
+        </div>
     </div>
   )
 }
